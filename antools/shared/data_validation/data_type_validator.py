4e0c59b29fc19288d12f05cd052ea39fc969ed6b
adrozda <adrozda.dev@gmail.com>
2021-04-10 20:59:44 +0200

    Update data_type_validator.py

--- antools/shared/data_validation/data_type_validator.py
+++ antools/shared/data_validation/data_type_validator.py
@@ -21,40 +21,110 @@ __date__ = "28/02/2021"
 # %% FILE IMPORT
 
 class DataTypeValidator():
-    
-    def __init__(self):
-        pass
+    """ Class used for handling various datatypes and known objects and user requirements on them 
+    TO BE FINISHED """
     
     def __call__(self, input):
+        """ Returns as same output as function type() """
         return type(input)
     
-    def str(self, input, reason:bool = False, none_allowed = False) -> tuple or bool:
+    def str(self, input, reason:bool = False,
+            none_allowed:bool = False,
+            exceptions:list = [],
+            forbidden:list = [],
+            max_len:int = None, 
+            min_len:int = None,
+            only_ascii:bool = None) -> tuple or bool:
+        """ TO BE FINISHED """
         
-        if none_allowed and input is None:
+        # check none
+        return_none = self._check_none(input) if none_allowed else False
+        if return_none:
             return self._report(reason=reason)
+        
+        # check exceptions
+        if exceptions and isinstance(exceptions, list):
+            if input in exceptions:
+               return self._report(reason=reason)
+                              
+        # check forbidden
+        if forbidden and isinstance(forbidden, list):
+            if input in forbidden:
+               return self._report("VALUE IS FORBIDDEN", reason=reason)
+               
+        # check length
+        if isinstance(max_len, int):
+            if len(input) > max_len and max_len > 0:
+                return self._report(f"STRING IS TOO LONG ({len(input)}/{max_len})", reason=reason)
             
-        return self._report(False, "NOT A STRING", reason) if not isinstance(input, str) else self._report(reason=reason)
+        if isinstance(min_len, int):
+            if len(input) < min_len and min_len > 0:
+                return self._report(f"STRING IS TOO SHORT ({len(input)}/{min_len})", reason=reason)
+        
+        # check ascii
+        if only_ascii:
+            if not all(ord(c) < 128 for c in input):
+                return self._report("STRING CONTAINS NON ASCII CHARACTERS", reason=reason)
+            
+        return self._report("NOT A STRING", reason=reason) if not isinstance(input, str) else self._report(reason=reason)
 
-    def int(self, input, reason:bool = False) -> tuple or bool:
-        return self._report(False, "NOT AN INTEGER", reason) if not isinstance(input, int) else self._report(reason=reason)
-    
-    def bool(self, input, reason:bool = False) -> tuple or bool:
-        return self._report(False, "NOT A BOOLEAN", reason) if not isinstance(input, bool) else self._report(reason=reason)  
-    
-    def object(self, input, reason:bool = False, class_name:str = None):
-        if not isinstance(input, object):
-            return self._report(False, "NOT AN OBJECT", reason)
+
+    def int(self, input, reason:bool = False,
+            none_allowed:bool = False,
+            exceptions:list = [],
+            forbidden:list = [],
+            max_value:int = None, 
+            min_value:int = None) -> tuple or bool:
+        """ TO BE FINISHED """
+        
+
             
-        if class_name and (class_name != input.__class__.__name__):
-            return self._report(False, f"NOT AN INSTANCE OF CLASS <{class_name}> BUT IS INSTANCE OF CLASS <{input.__class__.__name__}>", reason)
+        # check none
+        return_none = self._check_none(input) if none_allowed else False
+        if return_none:
+            return self._report(reason=reason)
+        
+        # check exceptions
+        if exceptions and type(exceptions) == list:
+            if input in exceptions:
+               return self._report(reason=reason)
+                           
+        # check forbidden
+        if forbidden and type(forbidden) == list:
+            if input in forbidden:
+               return self._report("VALUE IS FORBIDDEN", reason=reason)
+               
+        # check length
+        if isinstance(max_value, int):
+            if input > max_value:
+                return self._report(f"INTEGER IS TOO BIG ({input}>{max_value})", reason=reason)
+            
+        # check length
+        if isinstance(min_value, int):
+            if input < min_value:
+                return self._report(f"INTEGER IS TOO SMALL ({input}<{min_value})", reason=reason)
+        
+        # special case to avoid booleans mix -> Python definition: isinstance(False, int) == True
+        if isinstance(input, bool):
+            return self._report("NOT AN INTEGER", reason=reason)
+             
+        return self._report("NOT AN INTEGER", reason=reason) if not isinstance(input, int) else self._report(reason=reason)    
     
-        return self._report(reason=reason)  
-    
-    def _report(self, valid:bool = True, cause:str = None, reason:bool = False) -> tuple or bool:
+
+
+    def _report(self, cause:str = None, reason:bool = False) -> tuple or bool:
+        valid = False if cause else True
         return (valid, cause) if reason else valid
+    
+    def _check_none(self, input):
+        """ TO BE EXTENDED by np.nan, NULL etc. """
+        return True if input == None else False 
+            
+ 
 
 # create instance of the class
 TypeValidator = DataTypeValidator()
+x = TypeValidator.str("null", reason=True, none_allowed=True, forbidden=["null"])
 # %% INPUTS
 
 # %% CLASSES
