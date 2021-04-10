902aea55aea6609bfb66501e5cdcdcf58eb0a02b
adrozda <adrozda.dev@gmail.com>
2021-04-10 09:00:23 +0200

    rewrited module setup

--- antools/__init__.py
+++ antools/__init__.py
@@ -2,25 +2,30 @@
 ANTOOLS - ANTON'S TOOLS
 
 @ author: Antonín Drozda
-@ organization: Freelancer
-@ project: Anton's Tools
+@ author email: adrozda@dev.gmail.com
 @ contact:  Email    (adrozda.dev@gmail.com)
             GitHub   (https://github.com/antonin-drozda)
             StackOverFlow (https://stackoverflow.com/users/14750652/anton%c3%adn-drozda?tab=profile)           
-            LinkedIn (https://linkedin.com/in/anton%C3%ADn-drozda-a372b116a/)                      
-@ license: MIT
-@ copyright: Copyright (C) 2021 Antonín Drozda
-@ date: 06/04/2021
-@ version: 2021.4.1
+            LinkedIn (https://linkedin.com/in/anton%C3%ADn-drozda-a372b116a/) 
+@ url: https://github.com/antonin-drozda
+@ organization: Freelancer
 
-Library used as useful personal code keeper also open to the public
+@ project: Anton's Tools
+@ version: 2021.4.1
+@ copyright: Copyright (C) 2021 Antonín Drozda
+@ license: MIT
+@ date: 06/04/2021
+
+Private library which is also free to public use. Its purpose is to be useful code keeper.
 
 """
 
 # %% FILE METADATA
 __title__ = "ANTON'S TOOLS"
 __author__ = "Antonin Drozda"
+__author_email__ = "adrozda.dev@gmail.com"
 __contact__ = "adrozda.dev@gmail.com"
+__url__ = "https://github.com/antonin-drozda"
 __organization__ = "Freelancer"
 
 __project__ = "Anton's Tools"
@@ -28,10 +33,16 @@ __version__ = "2021.4.1"
 __copyright__ = "Copyright (C) 2021 Antonín Drozda"
 __license__= "MIT"
 __date__ = "06/04/2021"
+__description__ = "Private library which is also free to public use. Its purpose is to be useful code keeper."
 
 
 # %% ADDITIONAL PYPI METADATA
-URL = "https://github.com/antonin-drozda"
-DESCRIPTION = "Private library which is also free to public use. Its purpose is to be useful code keeper."
-PACKAGE_DEPENDENCY = ['pandas >= 1.2.2', 'psycopg2 >= 2.8.6', 'sqlalchemy >= 1.3.23']
-KEYWORDS = ['antools', 'database', 'postgres', 'logger', 'logging', 'data science', 'data validation']
+PACKAGE_DEPENDENCY = ['pandas >= 1.2.2', 
+                      'psycopg2 >= 2.8.6', 
+                      'sqlalchemy >= 1.3.23']
+
+KEYWORDS = ['antools', 
+            'database', 'postgres', 
+            'logger', 'logging', 
+            'data science', 'data validation', 
+            'system']
