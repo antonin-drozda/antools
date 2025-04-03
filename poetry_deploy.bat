cce929d6eee52337a858a552fa449dc6272d300f
Antonín <adrozda.dev@gmail.com>
2025-04-03 20:50:20 +0200

    published antools v 2.8.3

--- poetry_deploy.bat
+++ poetry_deploy.bat
@@ -10,64 +10,11 @@ cd /d "%~dp0"
 :: Define virtual environment directory
 set VENV_DIR=venv
 
-:: Check if virtual environment exists, if not, create it
-if not exist "%VENV_DIR%" (
-    echo Creating virtual environment...
-    python -m venv "%VENV_DIR%"
-)
-
-:: Activate virtual environment
-echo Activating virtual environment...
-call "%VENV_DIR%\Scripts\activate"
-
-:: Upgrade pip
-echo Upgrading pip...
-python -m pip install --upgrade pip
-
-:: Upgrade pip and install necessary libraries
-echo Installing required libraries...
-pip install poetry
-poetry init
-poetry install
-
-:: Ensure dist/ directory is clean before build
-if exist dist\* (
-    echo Cleaning up old build files...
-    del /Q dist\*
-)
-
-:: Optional: Clear the Poetry cache (remove old cached packages)
-poetry cache clear pypi --all
-
-:: Optionally, you can add a message or pause to indicate completion
-echo Old Poetry builds removed and Poetry cache cleared.
-
-:: Build the package
-echo Building the package...
-poetry build
-
-:: Install package locally for testing
-echo Installing the package locally for testing...
-pip uninstall -y %LIBRARY_NAME%
-pip install .
-
-:: Open installed library in venv (if it exists)
-set LIB_PATH="%VENV_DIR%\Lib\site-packages\%LIBRARY_NAME%"
-if exist %LIB_PATH% (
-    echo Opening installed library folder...
-    explorer %LIB_PATH%
-) else (
-    echo WARNING: Library folder not found inside venv.
-)
-
 :: Ask user for confirmation before uploading
-set /p CONFIRM="Do you want to publish to PyPI? (y/N): "
+set /p CONFIRM="Do you want to publish to PyPI and push to Git? (y/N): "
 
 if /i "%CONFIRM%"=="y" (
-    :: Upload to PyPI using Poetry's built-in publish command
-    echo Uploading package to PyPI...
-    poetry publish --build
-
+    
     :: Open the URL in the default browser
     start https://pypi.org/project/%LIBRARY_NAME%/#history
 
@@ -77,20 +24,12 @@ if /i "%CONFIRM%"=="y" (
     :: Pause to view output (optional)
     pause
 
-    :: Provide additional help if the user needs to set the token
+     :: Provide additional help if the user needs to set the token
     echo If not working, call: poetry config pypi-token.pypi YOUR_TOKEN
 
     :: Inform about deployment completion
     echo Deployment completed!
 
-) else (
-    echo Upload canceled. Please check your package before publishing.
-)
-
-:: Ask user for confirmation to commit to Git
-set /p CONFIRM="Do you want to create commit to Git? (y/N): "
-
-if /i "%CONFIRM%"=="y" (
     :: Get library version dynamically from the first file found in dist/
     cd dist
 
@@ -104,10 +43,8 @@ if /i "%CONFIRM%"=="y" (
     :: Extract the version number from the filename (e.g., "your_package-0.1.0-py3-none-any.whl")
     for /f "tokens=2 delims=-" %%v in ("!package_file!") do (
         set "version=%%v"
-        goto :done
     )
 
-    :done
     :: Display the extracted version
     echo Version: !version!
 
@@ -135,9 +72,9 @@ if /i "%CONFIRM%"=="y" (
     :: Pause to view output (optional)
     pause
 
+
 ) else (
-    echo GIT commit not created.
-    echo No changes were committed to GitHub.
+    echo Aborted.
 )
 
 :: End of script
